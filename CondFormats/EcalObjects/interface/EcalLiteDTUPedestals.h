a97e154f091999d8e8ca3259ed47d659b37115b5
Dario <dario.soldi@cern.ch>
2019-03-20 11:11:15 +0100

    Add EcalPedestals producer

---
+++ CondFormats/EcalObjects/interface/EcalLiteDTUPedestals.h
@@ -0,0 +1,49 @@
+#ifndef EcalLiteDTUPedestals_h
+#define EcalLiteDTUPedestals_h
+
+
+#include "CondFormats/Serialization/interface/Serializable.h"
+#include "CondFormats/EcalObjects/interface/EcalCondObjectContainer.h"
+
+const int NGAINS=2;
+
+const int gainId1=1;
+const int gainId10=0;
+
+class EcalLiteDTUPed  {
+
+ public:
+  double meanarray[2]={110.,101};
+  double rmsarray[2]={1.10,1.01};
+ 
+  int setMean(int i, float value){
+    if(i>NGAINS || i<0) return -1;
+    else
+      meanarray[i] = value;
+    return 1;
+  }
+
+
+  int setRMS(int i,float value){
+    if(i > NGAINS || i < 0) return -1;
+    else
+      rmsarray[i] = value;
+    return 1;
+  }
+
+  float mean(int i) const {
+    return meanarray[i];
+  }
+
+  float rms(int i) const {
+    return rmsarray[i];
+  }
+
+  COND_SERIALIZABLE;
+};
+
+typedef EcalCondObjectContainer<EcalLiteDTUPed> EcalLiteDTUPedestalsMap;
+typedef EcalLiteDTUPedestalsMap::const_iterator EcalLiteDTUPedestalsMapIterator;
+typedef EcalLiteDTUPedestalsMap EcalLiteDTUPedestals;
+
+#endif
